97d401da759f11ae49383c7af73f769ee2d7f0b0
bimal-creatives <72511459+bimal-creatives@users.noreply.github.com>
2022-04-05 15:17:27 +0530

    changed

---
+++ main.py
@@ -0,0 +1,43 @@
+from pickle import TRUE
+from tkinter import SCROLL, scrolledtext
+import pygame
+import math
+
+pygame.init()
+
+clock = pygame.time.Clock()
+FPS = 60
+
+
+SCREEN_WIDTH = 1500
+SCREEN_HEIGHT = 600
+
+screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+pygame.display.set_caption('itz mario!!')
+
+bg = pygame.image.load('bg.png').convert()
+bg_width = bg.get_width()
+
+scroll = 0
+tiles = math.ceil ( SCREEN_WIDTH/ bg_width) + 1
+
+run = TRUE
+while run:
+
+    clock.tick(FPS)
+
+    for i in range (0,tiles):
+
+        screen.blit(bg,(i*bg_width + scroll,0))
+    
+    scroll -= 5
+    if abs(scroll) > bg_width:
+        scroll=0
+
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            run = False
+    
+    pygame.display.update()
+
+pygame.quit()
